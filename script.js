a01a89fcc401f629e01a0981e4e62236847f2353
thembu2005 <thendorakhi@gmail.com>
2021-08-29 12:22:28 +0200

    added new ui

--- script.js
+++ script.js
@@ -1,46 +1,30 @@
 let input = document.getElementById('city');
+let result;
 
 let submitContent = () => {
 
-fetch(`https://api.openweathermap.org/data/2.5/weather?q=${input.value}&appid=f10bfe1d0632bfeca9c074f70735b293`)
-.then((res) => res.json())
-.then((data) => {
-
-	let output = `
-	<h1> ${data.name} conditions </h1>
-	<hr>
-	<table class = "content">
-	<thead >
-	<tr>
-	<th>NAME</th>
-	<th> HUMIDITY </th>
-	<th>DESCRIPTION</th>
-	<th>TEMP</th>
-	<th>WINDSPEED </th>
-	<th> PRESSURE </th>
-	<th> SKY </th>
-	</tr>
-	<thead>
-	<tbody>
-	<tr>
-	<td data-label = "NAME">${data.name}</td>
-	<td data-label = "HUMIDITY"> ${data.main.humidity}</td>
-	<td data-label = "DESCRIPTION"> ${data.weather[0].description}</td>
-	<td data-label = "TEMP"> ${Math.round(data.main.temp - 273.15)} </td>
-	<td data-label = "WIND SPEED"> ${data.wind.speed} </td>
-	<td data-label = "PRESSURE"> ${data.main.pressure}</td>
-	<td data-label = "SKY"> ${data.weather[0].main}
-	</tr>
-	</table>
-	
-	`
-
-	document.getElementById('result').innerHTML = output;
-
-	console.log(data);
-})
-.catch((err) => document.getElementById('result').innerHTML = "Enter correct name")
-
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${input.value}&appid=db55fd495de05a4013318dac00c8b2ae`)
+        .then((res) => res.json())
+        .then((data) => {
+            result = `
+           <div id="container">
+            <header id="banner"><h1>${data.name} Conditions</h1></header>
+            <main id="content">
+            <div id="icon"><img src="http://openweathermap.org/img/w/${data.weather[0].icon}.png" height = '100px' width = '100px' alt="icon here"></div>
+            <div id="weather">
+            <ul>
+            <li> Descripion :${data.weather[0].description}</li>
+            <li> Humidty : ${data.main.humidity}</li>
+            <li> Temperature :${Math.round(data.main.temp - 273.15)}</li>
+            <li> Clouds : ${data.weather[0].main}</li>
+          </ul>
+            </div>
+            </main>
+             </div>
+             `
+            document.getElementById('result').innerHTML = result
+        })
+        .catch((err) => {
+            document.getElementById('result').innerHTML = 'enter a real city 😑'
+        })
 }
-
-
